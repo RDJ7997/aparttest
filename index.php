3d8d3e0629dc0eea13ba1b5fb5a63150df231739
RDJ7997 <135043783+RDJ7997@users.noreply.github.com>
2025-04-03 20:18:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    
+ <script defer data-domain="apar.com/S4Wp67" src="https://api.publytics.net/js/script.manual.min.js"></script>
+<script>
+    window.publytics = window.publytics || function() { (window.publytics.q = window.publytics.q || []).push(arguments) };
+    publytics('pageview');
+</script>
+</head>
+<body>
+    <h1>Welcome to my website</h1>
+    
+    <script>
+        setTimeout(function() {
+            window.location.href = "https://www.zillow.com";
+        }, 5000);
+    </script>
+</body>
+</html>
